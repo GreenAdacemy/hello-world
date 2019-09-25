3dc7c5ae13d2ca631fce60dc5b59a60d05811f84
Phuong <phuong@Admins-MacBook-Pro.local>
2019-09-25 17:41:56 +0700

    use map for array images

--- src/components/slide/silde.js
+++ src/components/slide/silde.js
@@ -5,7 +5,8 @@ import { Fade } from 'react-slideshow-image'
 const fadeImages = [
   'https://www.petmd.com/sites/default/files/senior-golden-retriever-with-ball-picture-id488657289.jpg',
   'https://nevadahumanesociety.org/wp-content/uploads/2018/08/NHS-Slider-03-1024x731.jpg',
-  'https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'
+  'https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+  'https://thumbs-prod.si-cdn.com/ttQ_J3nAhFMEyzSgmZOkfIEOzzs=/800x600/filters:no_upscale()/https://public-media.si-cdn.com/filer/5b/3e/5b3eefc6-3a12-45ad-8a46-f3ab1ab28901/istock-953069774.jpg'
 ]
 
 const fadeProperties = {
@@ -23,7 +24,18 @@ const SlideShow = () => {
   return (
     <div className='slide-container'>
       <Fade {...fadeProperties}>
-        <div className="each-fade">
+        {
+          fadeImages.map(
+            (imgUrl, index) => (
+              <div className="each-fade">
+                <div className="image-container">
+                  <img src={imgUrl} />
+                </div>
+              </div>    
+            )
+          )
+        }
+        {/* <div className="each-fade">
           <div className="image-container">
             <img src={fadeImages[0]} />
           </div>
@@ -37,7 +49,7 @@ const SlideShow = () => {
           <div className="image-container">
             <img src={fadeImages[2]} />
           </div>
-        </div>
+        </div> */}
       </Fade>
     </div>
   )
